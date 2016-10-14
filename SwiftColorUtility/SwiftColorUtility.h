eeadd5fd2bb5ead2e0c17a168b2eb5be9607eb34
Alexander Nazarov <nazavrik@gmail.com>
2016-10-14 19:09:45 -0400

    Initial commit.

---
+++ SwiftColorUtility/SwiftColorUtility.h
@@ -0,0 +1,19 @@
+//
+//  SwiftColorUtility.h
+//  SwiftColorUtility
+//
+//  Created by alexander on 14.10.16.
+//  Copyright © 2016 nazavrik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftColorUtility.
+FOUNDATION_EXPORT double SwiftColorUtilityVersionNumber;
+
+//! Project version string for SwiftColorUtility.
+FOUNDATION_EXPORT const unsigned char SwiftColorUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftColorUtility/PublicHeader.h>
+
+
